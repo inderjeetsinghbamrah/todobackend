3d834b94d958662b618c3c042580e0173bb0598e
Inderjeet Singh <inderjeet.s.bamrah@gmail.com>
2024-04-20 07:16:28 +0530

    CORS updated

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(express.json());
 
-app.use(cors({ origin: "https://fantastic-crumble-d100a5.netlify.app/" }));
+app.use(cors({}));
 
 app.post("/todo", async (req, res) => {
   const title = req.body.title;
